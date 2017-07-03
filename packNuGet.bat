30c9536efdbeb78735f25eb95a15e6c5cd2a1fd0
ChazSII <ChazSII@users.noreply.github.com>
2017-07-03 16:43:02 -0400

    Delete packNuGet.bat
    Unnecessary file

--- packNuGet.bat
+++
@@ -1,1 +0,0 @@
-C:\programdata\chocolatey\bin\nuget pack "TrueCryptManagedDriver" -Prop Configuration=Release;Platform=x64;iconUrl=http://upload.wikimedia.org/wikipedia/en/b/b8/TrueCrypt_Logo.png;projectUrl=https://github.com/ChazSII/TrueCrypt-Managed-Driver
